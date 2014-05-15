82cd7222559525097341febb9dd0bc2b952ad406
void <git.email.address@googlemail.com>
2014-05-15 09:48:39 +0200

    Added support for excluding patterns

--- AGitation/Application.cs
+++ AGitation/Application.cs
@@ -10,26 +10,29 @@ namespace AGitation
 {
 	class Application
 	{
-		static void AnalyseRepository(string repositoryPath, string outputPath, Regex pattern)
+		static void AnalyseRepository(string repositoryPath, string outputPath, Regex inclusivePattern, Regex exclusivePattern)
 		{
 			long totalLineCount = 0;
 			var results = new Dictionary<string, long>();
 			using (var repository = new Repository(repositoryPath))
 			{
 				var files = Directory.GetFiles(repositoryPath, "*", SearchOption.AllDirectories);
-				var targets = files.Where(file => pattern.Match(file).Success);
+        var targets = files.Select(path =>
+          {
+            path = path.Substring(repositoryPath.Length);
+            path = path.Replace("\\", "/");
+            if (path.Length > 0 && path[0] == '/')
+              path = path.Substring(1);
+            return path;
+          });
+        targets = targets.Where(path => inclusivePattern.Match(path).Success && !exclusivePattern.Match(path).Success);
 				int filesProcessed = 0;
 				int fileCount = targets.Count();
-				foreach (var file in targets)
+				foreach (var path in targets)
 				{
 					if (filesProcessed % 10 == 0)
 						Console.WriteLine("Files: {0}/{1}, authors: {2}, lines: {3}", filesProcessed, fileCount, results.Keys.Count, totalLineCount);
-					if (!pattern.Match(file).Success)
-						continue;
-					string path = file.Substring(repositoryPath.Length);
-					path = path.Replace("\\", "/");
-					if (path.Length > 0 && path[0] == '/')
-						path = path.Substring(1);
+          Console.WriteLine("Processing {0}", path);
 					try
 					{
 						var blameHunks = repository.Blame(path);
@@ -65,17 +68,18 @@ namespace AGitation
 
 		static void Main(string[] arguments)
 		{
-			if (arguments.Length != 3)
+			if (arguments.Length != 4)
 			{
 				string name = Assembly.GetExecutingAssembly().ManifestModule.Name;
 				Console.WriteLine("Usage:");
-				Console.WriteLine("{0} <path to the root of a git repository> <output path> <regular expression specifying which paths to analyse>", name);
+				Console.WriteLine("{0} <path to the root of a git repository> <output path> <regular expression specifying which paths to include> <regular expression specyfing which paths to exclude>", name);
 				return;
 			}
 			string repositoryPath = arguments[0];
 			string outputPath = arguments[1];
-			var pattern = new Regex(arguments[2]);
-			AnalyseRepository(repositoryPath, outputPath, pattern);
+			var inclusivePattern = new Regex(arguments[2]);
+      var exclusivePattern = new Regex(arguments[3]);
+			AnalyseRepository(repositoryPath, outputPath, inclusivePattern, exclusivePattern);
 		}
 	}
 }
