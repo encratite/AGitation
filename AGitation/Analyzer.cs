74af8261cfb9203caaeba8602da0da0812d3ddab
void <git.email.address@googlemail.com>
2014-05-20 17:11:45 +0200

    Added threading support

---
+++ AGitation/Analyzer.cs
@@ -0,0 +1,119 @@
+﻿using LibGit2Sharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace AGitation
+{
+    class Analyzer
+    {
+        string _RepositoryPath;
+        string _OutputPath;
+        Regex _InclusivePattern;
+        Regex _ExclusivePattern;
+
+        IEnumerable<string> _RemainingTargets;
+        Dictionary<string, long> _Results;
+        long _TotalLineCount;
+        int _FilesProcessed;
+        int _FileCount;
+
+        public Analyzer(string repositoryPath, string outputPath, Regex inclusivePattern, Regex exclusivePattern)
+        {
+            _RepositoryPath = repositoryPath;
+            _OutputPath = outputPath;
+            _InclusivePattern = inclusivePattern;
+            _ExclusivePattern = exclusivePattern;
+        }
+
+        public void Run()
+        {
+            _Results = new Dictionary<string, long>();
+            _TotalLineCount = 0;
+            var files = Directory.GetFiles(_RepositoryPath, "*", SearchOption.AllDirectories);
+            _RemainingTargets = files.Select(path =>
+            {
+                path = path.Substring(_RepositoryPath.Length);
+                path = path.Replace("\\", "/");
+                if (path.Length > 0 && path[0] == '/')
+                    path = path.Substring(1);
+                return path;
+            });
+            _RemainingTargets = _RemainingTargets.Where(path => _InclusivePattern.Match(path).Success && !_ExclusivePattern.Match(path).Success);
+            _FilesProcessed = 0;
+            _FileCount = _RemainingTargets.Count();
+            var threads = new List<Thread>();
+            int threadCount = Environment.ProcessorCount;
+            Console.WriteLine("Using {0} thread(s)", threadCount);
+            for (int i = 0; i < threadCount; i++)
+            {
+                var thread = new Thread(BlameThread);
+                thread.Start();
+                threads.Add(thread);
+            }
+            foreach (var thread in threads)
+                thread.Join();
+            using (var outputStream = new StreamWriter(_OutputPath))
+            {
+                var pairs = _Results.Select(pair => pair).OrderByDescending(pair => pair.Value);
+                foreach (var pair in pairs)
+                {
+                    string author = pair.Key;
+                    long lineCount = pair.Value;
+                    double percentage = (double)lineCount / _TotalLineCount * 100.0;
+                    outputStream.WriteLine("{0}: {1:0.0}% ({2})", author, percentage, lineCount);
+                }
+            }
+        }
+
+        void BlameThread()
+        {
+            using (var repository = new Repository(_RepositoryPath))
+            {
+                bool firstTime = true;
+                while (true)
+                {
+                    string path;
+                    lock (this)
+                    {
+                        if (firstTime)
+                            firstTime = false;
+                        else
+                            _FilesProcessed++;
+                        if (_RemainingTargets.Count() == 0)
+                            return;
+                        path = _RemainingTargets.First();
+                        _RemainingTargets = _RemainingTargets.Skip(1);
+                        if (_FilesProcessed % 10 == 0)
+                            Console.WriteLine("Files: {0}/{1}, authors: {2}, lines: {3}", _FilesProcessed, _FileCount, _Results.Keys.Count, _TotalLineCount);
+                        Console.WriteLine("Processing {0}", path);
+                    }
+                    try
+                    {
+                        var blameHunks = repository.Blame(path);
+                        foreach (var blameHunk in blameHunks)
+                        {
+                            string author = blameHunk.FinalSignature.Name;
+                            int lineCount = blameHunk.LineCount;
+                            lock (this)
+                            {
+                                _TotalLineCount += lineCount;
+                                if (!_Results.ContainsKey(author))
+                                    _Results[author] = 0;
+                                _Results[author] += lineCount;
+                            }
+                        }
+                    }
+                    catch
+                    {
+                        // This path is likely not part of the history
+                    }
+                }
+            }
+        }
+    }
+}
--- AGitation/Application.cs
+++ AGitation/Application.cs
@@ -8,78 +8,23 @@ using System.Text.RegularExpressions;
 
 namespace AGitation
 {
-	class Application
-	{
-		static void AnalyseRepository(string repositoryPath, string outputPath, Regex inclusivePattern, Regex exclusivePattern)
-		{
-			long totalLineCount = 0;
-			var results = new Dictionary<string, long>();
-			using (var repository = new Repository(repositoryPath))
-			{
-				var files = Directory.GetFiles(repositoryPath, "*", SearchOption.AllDirectories);
-        var targets = files.Select(path =>
-          {
-            path = path.Substring(repositoryPath.Length);
-            path = path.Replace("\\", "/");
-            if (path.Length > 0 && path[0] == '/')
-              path = path.Substring(1);
-            return path;
-          });
-        targets = targets.Where(path => inclusivePattern.Match(path).Success && !exclusivePattern.Match(path).Success);
-				int filesProcessed = 0;
-				int fileCount = targets.Count();
-				foreach (var path in targets)
-				{
-					if (filesProcessed % 10 == 0)
-						Console.WriteLine("Files: {0}/{1}, authors: {2}, lines: {3}", filesProcessed, fileCount, results.Keys.Count, totalLineCount);
-          Console.WriteLine("Processing {0}", path);
-					try
-					{
-						var blameHunks = repository.Blame(path);
-						foreach (var blameHunk in blameHunks)
-						{
-							string author = blameHunk.FinalSignature.Name;
-							int lineCount = blameHunk.LineCount;
-							totalLineCount += lineCount;
-							if (!results.ContainsKey(author))
-								results[author] = 0;
-							results[author] += lineCount;
-						}
-					}
-					catch
-					{
-						// This path is likely not part of the history
-					}
-					filesProcessed++;
-				}
-			}
-			using (var outputStream = new StreamWriter(outputPath))
-			{
-				var pairs = results.Select(pair => pair).OrderByDescending(pair => pair.Value);
-				foreach (var pair in pairs)
-				{
-					string author = pair.Key;
-					long lineCount = pair.Value;
-					double percentage = (double)lineCount / totalLineCount * 100.0;
-					outputStream.WriteLine("{0}: {1:0.0}% ({2})", author, percentage, lineCount);
-				}
-			}
-		}
-
-		static void Main(string[] arguments)
-		{
-			if (arguments.Length != 4)
-			{
-				string name = Assembly.GetExecutingAssembly().ManifestModule.Name;
-				Console.WriteLine("Usage:");
-				Console.WriteLine("{0} <path to the root of a git repository> <output path> <regular expression specifying which paths to include> <regular expression specyfing which paths to exclude>", name);
-				return;
-			}
-			string repositoryPath = arguments[0];
-			string outputPath = arguments[1];
-			var inclusivePattern = new Regex(arguments[2]);
-      var exclusivePattern = new Regex(arguments[3]);
-			AnalyseRepository(repositoryPath, outputPath, inclusivePattern, exclusivePattern);
-		}
-	}
+    class Application
+    {
+        static void Main(string[] arguments)
+        {
+            if (arguments.Length != 4)
+            {
+                string name = Assembly.GetExecutingAssembly().ManifestModule.Name;
+                Console.WriteLine("Usage:");
+                Console.WriteLine("{0} <path to the root of a git repository> <output path> <regular expression specifying which paths to include> <regular expression specyfing which paths to exclude>", name);
+                return;
+            }
+            string repositoryPath = arguments[0];
+            string outputPath = arguments[1];
+            var inclusivePattern = new Regex(arguments[2]);
+            var exclusivePattern = new Regex(arguments[3]);
+            var analyzer = new Analyzer(repositoryPath, outputPath, inclusivePattern, exclusivePattern);
+            analyzer.Run();
+        }
+    }
 }
